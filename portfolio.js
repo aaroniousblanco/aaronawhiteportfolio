059f5e7f38024e7fb7ad265b57c5a1a462533a37
aaroniousblanco <aaronawhite1@gmail.com>
2017-04-19 15:18:52 -0400

    Update responsiveness and projects section.

--- portfolio.js
+++ portfolio.js
@@ -1,92 +1,132 @@
 $(document).ready(function() {
 
-  $("#about_section").hide(); //hides about section, so toggle works on click
+  $("#about_section").hide(); //lines 3-6 hide main content sections on initial page load, so toggle works on click
   $("#projects_section").hide();
-  $("#contact_section").hide();
+  $("#skills_section").hide();
   $("hr").hide();
 
-  $("#sidebar").click(function() { //on click, adds a class to the sidebar which slides it from the left using a CSS transition
-    $( "#sidebar" ).addClass("shown");
-    $("#home_page").addClass("down");
-    $("hr").show(4000);
+  $("#sidebar").click(function() { //this allows lines 9-11 to execute on page load, automatically
+    $( "#sidebar" ).addClass("shown"); //adds a class to the sidebar which slides it from the left using a CSS transition;
+    $("#home_page").addClass("down"); //adds a class to the home_page section which slides it down from the top automatically
+    $("hr").show(4000); //decorative horizontal rule line scrolls across the screen on page load and underlines "Aaron White"
   });
 
   $(function() { //triggers the above click automatically so the side bar slides in when the page initially loads
     $("#sidebar").click();
   });
 
-  $("#about").on("click", function() { //this toggles the about section, when the about link is clicked
-    $("#about_section").slideToggle(800, "linear", function() {
-    });
-    $("#projects_section").hide();
-    $("#contact_section").hide();
+  $(".about").on("click", function() { //this toggles the about section, when the about link is clicked
+    $("#about_section").show(200);
+    // slideToggle(800, "linear", function() {
+    // });
+    $("#projects_section").hide(); //21-23 hide content sections other than about section when it is clicked
+    $("#skills_section").hide();
     $("#home_page").hide();
-    if($(this).hasClass('left')){ //this if section slides the sidebar about item to the right and left
-        $(this).removeClass('left').addClass('right');
-        $("#about > a").removeClass("left").addClass('right');
-    }else if ($(this).hasClass('right')) {
-      $(this).removeClass('right').addClass('left');
-      $("#about > a").removeClass("right").addClass('left');
-      $("#home_page").show(800);
-    }else{
-        $(this).addClass('right');
+    if($("div.about").hasClass('left')){ //this if section slides the sidebar about item to the right and left
+        $("div.about").removeClass('left').addClass('right');
+        $(".about > a").removeClass("left").addClass('right');
+    } else if ($("div.about").hasClass('right')) {
+      $("div.about").removeClass('right').addClass('left');
+      $(".about > a").removeClass("right").addClass('left');
+      $("#about_section").hide(200);
+      $("#home_page").show(300);
     }
-    $("#projects, #contact, #projects > a, #contact > a").removeClass("right");
-    $("#contact, #contact > a, #projects, #projects > a").addClass("left");
+    $(".projects, .skills, .projects > a, .skills > a").removeClass("right");
+    $(".skills, .skills > a, .projects, .projects > a").addClass("left");
   });
 
-  $("#projects").on("click", function() { //this toggles the about section, when the about link is clicked
-    $("#projects_section").slideToggle(800, "linear", function() {
-    });
+  $(".projects").on("click", function() { //this toggles the about section, when the about link is clicked
+    $("#projects_section").show(200);
     $("#about_section").hide();
-    $("#contact_section").hide();
+    $("#skills_section").hide();
     $("#home_page").hide();
-    if($(this).hasClass('left')){ //this if section slides the sidebar about item to the right and left
-        $(this).removeClass('left').addClass('right');
-        $("#projects > a").removeClass("left").addClass('right');
-    }else if ($(this).hasClass('right')) {
-      $(this).removeClass('right').addClass('left');
-      $("#projects > a").removeClass("right").addClass('left');
-      $("#home_page").show(800);
-    }else{
-        $(this).addClass('right');
+    if($("div.projects").hasClass('left')){ //this if section slides the sidebar about item to the right and left
+        $("div.projects").removeClass('left').addClass('right'); //PER TOBY --> CAN PUT ALL OF THIS IN A FUNCTION AND REPEAT THIS SAME LOGIC BELOW FOR THE TOP NAVBAR OR RESTYLE THE LEFT NAVBAR INTO THE TOP NAVBAR USING A MEDIA QUERY
+        $(".projects > a").removeClass("left").addClass('right');
+    }else if ($("div.projects").hasClass('right')) {
+      $("div.projects").removeClass('right').addClass('left');
+      $(".projects > a").removeClass("right").addClass('left');
+      $("#projects_section").hide(200);
+      $("#home_page").show(300);
     }
-    $("#about, #contact, #about > a, #contact > a").removeClass("right");
-    $("#contact, #contact > a, #about, #about > a").addClass("left");
+    $(".about, .skills, .about > a, .skills > a").removeClass("right");
+    $(".skills, .skills > a, .about, .about > a").addClass("left");
   });
 
-  $("#contact").click(function() { //this toggles the about section, when the about link is clicked
-    $("#contact_section").slideToggle(800, "linear", function() {
-    });
+  $(".skills").click(function() { //this toggles the about section, when the about link is clicked
+    $("#skills_section").show(200);
     $("#about_section").hide();
     $("#projects_section").hide();
     $("#home_page").hide();
-    if($(this).hasClass('left')){ //this if section slides the sidebar about item to the right and left
-        $(this).removeClass('left').addClass('right');
-        $("#contact > a").removeClass("left").addClass('right');
-    }else if ($(this).hasClass('right')) {
-      $(this).removeClass('right').addClass('left');
-      $("#contact > a").removeClass("right").addClass('left');
-      $("#home_page").show(800);
-    }else{
-        $(this).addClass('right');
+    if($("div.skills").hasClass('left')){ //this if section slides the sidebar about item to the right and left
+        $("div.skills").removeClass('left').addClass('right');
+        $(".skills > a").removeClass("left").addClass('right');
+    }else if ($("div.skills").hasClass('right')) {
+      $("div.skills").removeClass('right').addClass('left');
+      $(".skills > a").removeClass("right").addClass('left');
+      $("#skills_section").hide(200);
+      $("#home_page").show(300);
     }
-    $("#about, #projects, #about > a, #projects > a").removeClass("right");
-    $("#projects, #projects > a, #about, #about > a").addClass("left");
+    $(".about, .projects, .about > a, .projects > a").removeClass("right");
+    $(".projects, .projects > a, .about, .about > a").addClass("left");
   });
 
-  var bounceInUp = true; //this section triggers the bounce animation for the linkedin, github, and resume icons when the contact link is clicked on the top navbar
-  $("#contact_top").on("click", function() {
+  var bounceInUp = true; //this section triggers the bounce animation for the linkedin, github, and resume icons when the contact link is clicked on the top navbar; uses a jQuery plugin called "animate"
+  $(".contact").on("click", function() {
     if (bounceInUp === true) {
-      $("#shake, #shake2, #shake3").removeClass('bounceInUp').addClass("bounce").delay(1000).queue(function(){
+      $("#shake, #shake2, #shake3, #shake4").removeClass('bounceInUp').addClass("bounce").delay(1000).queue(function(){
       $(this).removeClass("bounce").dequeue();
       });
       bounceInUp = false;
     } else if (bounceInUp === false){
-      $("#shake, #shake2, #shake3").addClass("bounce").delay(1000).queue(function(){
+      $("#shake, #shake2, #shake3, #shake4").addClass("bounce").delay(1000).queue(function(){
       $(this).removeClass("bounce").dequeue();
       });
       bounceInUp = true;
     }
   });
-});
+
+//BEGIN TOP NAVBAR SECTION FOR MOBILE SCREENS
+
+  $("#skills_top").on("click", function() {
+      $("#skills_section").animate({width:'toggle'}, 200);
+      $("#home_page").hide();
+      $("#about_section").hide();
+      $("#projects_section").hide();
+      if ($("#home_page").hasClass("down")) {
+        $("#home_page").removeClass("down");
+        $("#home_page").hide();
+      } else {
+        $("#home_page").addClass("down");
+        $("#home_page").show(500);
+      }
+  });
+
+  $("#projects_top").on("click", function() {
+      $("#projects_section").animate({width:'toggle'}, 200);
+      $("#home_page").hide();
+      $("#about_section").hide();
+      $("#skills_section").hide();
+      if ($("#home_page").hasClass("down")) {
+        $("#home_page").removeClass("down");
+        $("#home_page").hide();
+      } else {
+        $("#home_page").addClass("down");
+        $("#home_page").show(800);
+      }
+  });
+
+  $("#about_top").on("click", function() {
+      $("#about_section").animate({width:'toggle'}, 200);
+      $("#skills_section").hide();
+      $("#projects_section").hide();
+    if ($("#home_page").hasClass("down")) {
+      $("#home_page").removeClass("down");
+      $("#home_page").hide();
+    } else {
+      $("#home_page").addClass("down");
+      $("#home_page").show(800);
+    }
+  });
+
+}); //end of dom ready
